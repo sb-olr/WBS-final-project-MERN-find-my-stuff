ff9e2de12fb4ff53cbbe6df79fa890db17246da1
Sujit <sb2011ams@gmail.com>
2023-04-27 14:37:15 +0200

    test deploy

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,7 +18,7 @@ const Home = () => {
               Home Inventory
             </h2>
             <p className="text-gray-500 py-4 max-w-md">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet
+              Lorem ITEMS, dolor sit amet consectetur adipisicing elit. Amet
               iusto quisquam eius enim, deleniti assumenda rem sed fuga labore
               commodi.
             </p>
@@ -47,7 +47,7 @@ const Home = () => {
         </div>
       </div>
       <Features />
-        <Footer />
+      <Footer />
     </>
   );
 };
