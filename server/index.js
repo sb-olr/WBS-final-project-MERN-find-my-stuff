ce6744682cdb5c9f3f3c4fc1fb4e76aacf1d5523
Sujit <sb2011ams@gmail.com>
2023-04-28 11:44:06 +0200

    fake token from login

--- server/index.js
+++ server/index.js
@@ -3,18 +3,20 @@ const cors = require("cors");
 const morgan = require("morgan");
 require("dotenv").config();
 const usersRouter = require("./routes/users");
+const loginRouter = require("./routes/login");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
 
 app.use('/api/users', usersRouter);
+app.use("/api/login", loginRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
